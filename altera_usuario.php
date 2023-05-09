0b49be989a2334686a442e5b9e9701cc5b65f94f
KevinGGS <gomes.kevin@aluno.ifsp.edu.br>
2023-05-09 12:15:11 -0300

    sdafsdfsd

---
+++ altera_usuario.php
@@ -0,0 +1,44 @@
+<?php
+include('conexao.php');
+$id_usuario = $_GET['id_usuario'];
+$sql = "SELECT * FROM usuario where $id_usuario=$id_usuario";
+$result = mysqli_query($con,$sql);
+$row = mysqli_fetch_array($result);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Cadastro de usuarios - IFSP</h1>
+    
+    <form action="cadastro_usuario.php" method="post">
+    <div>
+        <label for="nome">Nome</label>
+        <input type="text" name="nome" id="nome"
+        value="<?php echo $row ['nome_usuario']?>">
+    </div>
+    <div>
+        <label for="email">E-mail</label>
+        <input type="text" name="email" id="email"
+        value="<?php echo $row ['email_usuario']?>">
+    </div>
+    <div>
+        <label for="fone">Telefone</label>
+        <input type="tel" name="fone" id="fone" placeholder="Formato (18) 9999-8888" pattern="\([0-9]{2}\)[0-9]{4-5}-[0-9]{4}"
+        value="<?php echo $row ['fone_usuario']?>">
+    </div>
+    <div>
+        <label for="senha">Senha</label>
+        <input type="text" name="senha" id="senha"
+        value="<?php echo $row ['senha']?>">
+    </div>
+    <input type="submit" value="Salvar">
+    </form>
+</body>
+</html>
--- listar_usuarios.php
+++ listar_usuarios.php
@@ -29,6 +29,7 @@
         echo"<td>". $row['nome_usuario'] ."</td>";
         echo"<td>". $row['email_usuario'] ."</td>";
         echo"<td>". $row['fone_usuario'] ."</td>";
+        echo "<td><a href='altera_usuario.php?id_usuario=" .$row['id_usuario']."'>alterar</a></td>";
         echo"</tr>";
         }while($row = mysqli_fetch_array($result))
         ?>
